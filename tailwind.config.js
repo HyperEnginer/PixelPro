d184d2a0ea102fb76d9ce98a0c715a07fbc1b9a4
Ahmet Faruk Saridanismet <asaridanismet#gmail.com>
2023-08-23 15:31:29 +0300

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    colors: {
+      "main-dark":"#0D0D0D",
+      "main-gray":"#30353F",
+      "main-light":"#FFFFFF",
+      "secondary-light":"#E4EDFF",
+      "main-blue":"#0554F1",
+      "secondary-blue":"#0540F2",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
