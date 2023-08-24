551da8ee7ad25a1a9a61ce4f088e5f04c39e86a0
Ahmet Faruk Saridanismet <asaridanismet#gmail.com>
2023-08-24 13:42:44 +0300

    Btn Component added

---
+++ src/Utility/Btn/Btn.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click.prevent="$emit('event')" class="bg-main-blue px-5 py-3 text-main-light font-medium shadow-btnShadow" >{{ text }}</button>
+</template>
+
+<script setup>
+const { text } = defineProps(['text'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
